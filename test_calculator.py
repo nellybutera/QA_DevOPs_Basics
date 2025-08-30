6e6f8fba2593adce881fc69180f22f872d0c62dd
nellybutera <buteranelly250@gmail.com>
2025-08-30 22:05:09 +0200

    removed the failing test

--- test_calculator.py
+++ test_calculator.py
@@ -7,5 +7,5 @@ def test_substract():
     assert substract(5,3) == 2
 
 # adding a failing test
-def test_failing_example():
-    assert add(2,2) == 5
+# def test_failing_example():
+#     assert add(2,2) == 5
