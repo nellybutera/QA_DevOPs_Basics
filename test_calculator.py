0911e6b5e92fcba65104ed96bdb4417b0b857009
nellybutera <buteranelly250@gmail.com>
2025-08-30 18:30:25 +0200

    my first tests

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+from calculator import add, substract
+
+def test_add():
+    assert add(2,3) == 5
+
+def test_substract():
+    assert substract(5,3) == 2
+
+# adding a failing test
+def test_failing_example():
+    assert add(2,2) == 5
